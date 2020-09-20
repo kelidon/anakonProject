2f269375174bea7e02cb121fca1a4263fa9a5669
kelidon <v.shchors@gmail.com>
2020-09-20 20:28:44 +0300

    Add scrolling to side images

--- lib/main.dart
+++ lib/main.dart
@@ -30,45 +30,101 @@ class Application extends StatelessWidget {
   }
 }
 
-class ApplicationPage extends StatelessWidget {
+class ApplicationPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _ApplicationPageState();
+}
+
+class _ApplicationPageState extends State<ApplicationPage> {
+  ScrollController _mainController;
+  ScrollController _blurController;
+  ScrollController _towerController;
+
+  _mainScrollListener() {
+    _towerController.jumpTo(_mainController.offset*0.1);
+    _blurController.jumpTo(_mainController.offset*0.03);
+  }
+
+  @override
+  void initState() {
+    _mainController = ScrollController();
+    _mainController.addListener(_mainScrollListener);
+    _towerController = ScrollController();
+    _blurController = ScrollController();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        drawer: DrawerWidget(),
-        appBar: AppBar(
-          toolbarHeight: 70,
-          backgroundColor: Colors.indigo.withOpacity(1),
-          leading: Builder(builder: (context) {
-            return IconButton(
-              icon: Icon(
-                Icons.menu,
-                size: 30,
-                color: Colors.white,
+      body: NotificationListener(
+        onNotification: (v) {
+          if (v is ScrollUpdateNotification) {}
+          return true;
+        },
+        child: Stack(
+          children: [
+            SingleChildScrollView(
+              controller: _towerController,
+              child: Container(
+                child: Image.asset(
+                  "assets/images/tower.jpg",
+                ),
               ),
-              onPressed: () {
-                if (Scaffold.of(context).isDrawerOpen) {
-                  Scaffold.of(context).openEndDrawer();
-                } else {
-                  Scaffold.of(context).openDrawer();
-                }
-              },
-            );
-          }),
-          actions: [
-            Container(
-                margin: EdgeInsets.only(right: 20),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Text("+375 (33) 354-76-45"),
-                    Text("anakon@gmail.com")
+            ),
+            SingleChildScrollView(
+              controller: _blurController,
+              child: Container(
+                alignment: Alignment.topRight,
+                child: Image.asset("assets/images/blur.jpg"),
+              ),
+            ),
+            Scaffold(
+                backgroundColor: Colors.transparent,
+                drawer: DrawerWidget(),
+                appBar: AppBar(
+                  backgroundColor: Colors.black.withOpacity(0.7),
+                  elevation: 0.0,
+                  toolbarHeight: 70,
+                  leading: Builder(builder: (context) {
+                    return IconButton(
+                      icon: Icon(
+                        Icons.menu,
+                        size: 30,
+                        color: Colors.white,
+                      ),
+                      onPressed: () {
+                        if (Scaffold.of(context).isDrawerOpen) {
+                          Scaffold.of(context).openEndDrawer();
+                        } else {
+                          Scaffold.of(context).openDrawer();
+                        }
+                      },
+                    );
+                  }),
+                  actions: [
+                    Container(
+                        margin: EdgeInsets.only(right: 20),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text("+375 (33) 354-76-45"),
+                            Text("anakon@gmail.com")
+                          ],
+                        ))
                   ],
-                ))
+                  title: Text(
+                    "Anakon",
+                    style: TextStyle(fontSize: 24),
+                  ),
+                  centerTitle: true,
+                ),
+                body: SingleChildScrollView(
+                    controller: _mainController, child: ContentWidget())),
           ],
-          title: Text("Anakon", style: TextStyle(fontSize: 24),),
-          centerTitle: true,
         ),
-        body: ContentWidget());
+      ),
+    );
   }
 }
--- lib/widgets/app_bar/drawer_widget.dart
+++ lib/widgets/app_bar/drawer_widget.dart
@@ -8,56 +8,60 @@ import 'drawer_button_widget.dart';
 class DrawerWidget extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return Drawer(
-      child: SingleChildScrollView(
-        child: Container(
-          margin: EdgeInsets.only(left: 15, top: 15),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              MouseRegion(
-                cursor: SystemMouseCursors.click,
-                child: GestureDetector(
-                  onTap: () => Navigator.of(context).pop(),
-                child: Icon(Icons.clear, size: 30, color: Colors.grey,),
+    return Container(
+      width: 250,
+      child: Drawer(
+        child: SingleChildScrollView(
+          child: Container(
+            margin: EdgeInsets.only(left: 15, top: 15),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                MouseRegion(
+                  cursor: SystemMouseCursors.click,
+                  child: GestureDetector(
+                    onTap: () => Navigator.of(context).pop(),
+                  child: Icon(Icons.clear, size: 30, color: Colors.grey,),
+                  ),
                 ),
-              ),
-              Container(
-                margin: EdgeInsets.only(left: 25, top: 30),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    DrawerButtonWidget(
-                      buttonName: DrawerButtons.ANAKON,
-                      isTitle: true,
-                      onTap: (){},
-                    ),
-                    SizedBox(
-                      height: 70,
-                    ),
-                    DrawerButtonWidget(
-                      buttonName: DrawerButtons.WHO_WE_ARE,
-                      onTap: (){},
-                    ),
-                    SizedBox(
-                      height: 35,
-                    ),
-                    DrawerButtonWidget(
-                      buttonName: DrawerButtons.SERVICES,
-                      onTap: (){},
-                    ),
-                    SizedBox(
-                      height: 35,
-                    ),
-                    DrawerButtonWidget(
-                      buttonName: DrawerButtons.CONTACT_US,
-                      onTap: (){},
-                    ),
+                Container(
+                  margin: EdgeInsets.only(left: 25, top: 30),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      InkWell(
+                        onTap: (){},
+                        child: Container(
+                          child:  Image.asset("assets/images/logo.png", scale: 2,),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 55,
+                      ),
+                      DrawerButtonWidget(
+                        buttonName: DrawerButtons.WHO_WE_ARE,
+                        onTap: (){},
+                      ),
+                      SizedBox(
+                        height: 35,
+                      ),
+                      DrawerButtonWidget(
+                        buttonName: DrawerButtons.SERVICES,
+                        onTap: (){},
+                      ),
+                      SizedBox(
+                        height: 35,
+                      ),
+                      DrawerButtonWidget(
+                        buttonName: DrawerButtons.CONTACT_US,
+                        onTap: (){},
+                      ),
 
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
