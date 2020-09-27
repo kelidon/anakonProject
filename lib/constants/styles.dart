d8792343c26af7873255e77e9064911f5d3d30e4
kelidon <v.shchors@gmail.com>
2020-09-27 23:48:09 +0300

    Add tower background video

--- lib/constants/styles.dart
+++ lib/constants/styles.dart
@@ -3,9 +3,13 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 
 class AppStyles {
-  static const TextStyle DRAWER_TITLE =
-      TextStyle(fontSize: 28, color: Colors.black, fontWeight: FontWeight.bold, height: 1.3);
-  static const TextStyle DRAWER_BUTTON = TextStyle(fontSize: 22, height: 1.3, color: Colors.grey);
+  static const TextStyle DRAWER_TITLE = TextStyle(
+      fontSize: 28,
+      color: Colors.black,
+      fontWeight: FontWeight.bold,
+      height: 1.3);
+  static const TextStyle DRAWER_BUTTON =
+      TextStyle(fontSize: 22, height: 1.3, color: Colors.grey);
   static const TextStyle DRAWER_BUTTON_ACTIVE = TextStyle(
     fontSize: 22,
     height: 1.3,
