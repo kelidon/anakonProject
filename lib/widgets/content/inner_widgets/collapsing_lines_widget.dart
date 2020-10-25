e3996d7fdf00b1b03c015fa566b25c9174103a4b
kelidon <v.shchors@gmail.com>
2020-10-26 00:08:56 +0300

    Add colors change on collapsed headers

--- lib/widgets/content/inner_widgets/collapsing_lines_widget.dart
+++ lib/widgets/content/inner_widgets/collapsing_lines_widget.dart
@@ -19,42 +19,53 @@ class _CollapsingWidgetState extends State<CollapsingLinesWidget> {
   _CollapsingWidgetState(this.titleType);
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-        onTap: () {
-          if(context.bloc<CollapsedHeadersBloc>().state.key == titleType){
-            if(context.bloc<CollapsedHeadersBloc>().state.value == CollapsingState.COLLAPSED)
-              context.bloc<CollapsedHeadersBloc>().add(ExpandEvent(titleType));
-            else{
-              context.bloc<CollapsedHeadersBloc>().add(CollapseEvent(titleType));
-            }
-          } else {
-              context.bloc<CollapsedHeadersBloc>().add(CollapseEvent(titleType));
-          }
+    return InkWell(onTap: () {
+      if (context.bloc<CollapsedHeadersBloc>().state.key == titleType) {
+        if (context.bloc<CollapsedHeadersBloc>().state.value ==
+            CollapsingState.COLLAPSED)
+          context.bloc<CollapsedHeadersBloc>().add(ExpandEvent(titleType));
+        else {
+          context.bloc<CollapsedHeadersBloc>().add(CollapseEvent(titleType));
+        }
+      } else {
+        context.bloc<CollapsedHeadersBloc>().add(CollapseEvent(titleType));
+      }
     }, child: BlocBuilder<CollapsedHeadersBloc,
         MapEntry<CollapsingTitle, CollapsingState>>(
       builder: (_, state) {
         bool isCollapsed = !(state.value == CollapsingState.EXPANDED);
+        bool isCurrent = state.key == titleType;
         return isCollapsed
             ? Container(
-          margin: EdgeInsets.only(bottom: 10),
+                margin: EdgeInsets.only(bottom: 10),
                 width: 50,
                 height: 50,
-                color: Colors.green,
-          child: Center(child: Icon(TypeToStateMapper.typeToStateMap[titleType].key)),
+                color: isCurrent ? Colors.blue : Colors.green,
+                child: Center(
+                    child: Icon(
+                  TypeToStateMapper.typeToStateMap[titleType].key,
+                  color: isCurrent ? Colors.white : Colors.blue,
+                )),
               )
             : Container(
-          margin: EdgeInsets.only(bottom: 10),
+                margin: EdgeInsets.only(bottom: 10),
                 width: double.maxFinite,
                 height: 50,
                 color: Colors.red,
-          child: Row(
-            children: [
-              Container(width: 50,
-                  height: 50,child: Center(child: Icon(TypeToStateMapper.typeToStateMap[titleType].key))),
-              SizedBox(width: 10,),
-              Text(TypeToStateMapper.typeToStateMap[titleType].value.key),
-            ],
-          ),
+                child: Row(
+                  children: [
+                    Container(
+                        width: 50,
+                        height: 50,
+                        child: Center(
+                            child: Icon(TypeToStateMapper
+                                .typeToStateMap[titleType].key))),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(TypeToStateMapper.typeToStateMap[titleType].value.key),
+                  ],
+                ),
               );
       },
     ));
