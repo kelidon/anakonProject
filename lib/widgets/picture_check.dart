c3b50c606295ae07dd4673b181a605370fbbaac4
kelidon <v.shchors@gmail.com>
2020-11-03 03:18:55 +0300

    save good memories

--- lib/widgets/picture_check.dart
+++ lib/widgets/picture_check.dart
@@ -1,8 +1,11 @@
 import 'dart:io';
 
+import 'package:anakonProject/bloc/collapsing_headers/collapsing_headers_bloc.dart';
+import 'package:anakonProject/bloc/metrics/metrics_bloc.dart';
 import 'package:anakonProject/constants/styles.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class PictureCheck extends StatefulWidget {
   @override
@@ -11,132 +14,113 @@ class PictureCheck extends StatefulWidget {
 
 class _PictureCheckState extends State<PictureCheck>
     with TickerProviderStateMixin {
-  bool isActive;
-  bool isActive1;
-  double width;
-  double height;
-  double width1;
-  double height1;
 
   @override
   Widget build(BuildContext context) {
-    final String assetName = 'assets/images/logo_on_tower_2.png';
+    final String assetName = 'assets/images/logo_menu.png';
 
-    return Container(
-      height: MediaQuery.of(context).size.height - 160,
-      child: Stack(
-        children: [
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Visibility(
-                visible: !isActive1,
-                child: AnimatedContainer(
-                  duration: Duration(milliseconds: 200),
-                  alignment: isActive ? Alignment.centerLeft : Alignment.center,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      AnimatedContainer(
-                        duration: Duration(milliseconds: 200),
-                        width: width,
-                        height: height,
-                        child: InkWell(
-                            onTap: () {
-                              setState(() {
-                                isActive = !isActive;
-                                if (isActive) {
-                                  isActive1 = false;
-                                  width = 400;
-                                  height = 400;
-                                } else {
-                                  width = 150;
-                                  height = 100;
-                                }
-                              });
-                            },
-                            child:
-                            Image.asset(assetName,
-                            )
-                        ),
-                      ),
-                      AnimatedOpacity(
-                          opacity: isActive?0:1,
-                          duration: Duration(milliseconds: 100),
-                          child: Text("title", style: AppStyles.TITLE,))
-                    ],
-                  ),
-                ),
-              ),
-              Visibility(
-                visible: !isActive,
-                child: AnimatedContainer(
-                  duration: Duration(milliseconds: 200),
-                  alignment: isActive1 ? Alignment.centerLeft : Alignment.center,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      AnimatedContainer(
-                        duration: Duration(milliseconds: 200),
-                        width: width1,
-                        height: height1,
-                        child: InkWell(
-                            onTap: () {
-                              setState(() {
-                                isActive1 = !isActive1;
-                                if (isActive1) {
-                                  isActive = false;
-                                  width1 = 400;
-                                  height1 = 400;
-                                } else {
-                                  width1 = 150;
-                                  height1 = 100;
-                                }
-                              });
-                            },
-                            child:
-                            Image.asset(assetName,
-                            )
-                        ),
-                      ),
-                      AnimatedOpacity(
-                          opacity: isActive1?0:1,
-                      duration: Duration(milliseconds: 100),
-                      child: Text("title", style: AppStyles.TITLE,))
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          ),
-          AnimatedOpacity(
-            opacity: isActive||isActive1?1:0,
-            duration: Duration(milliseconds: 100),
+    _buildPicture(CollapsingTitle titleType) {
+      return BlocBuilder<CollapsedHeadersConsBloc,
+          MapEntry<CollapsingTitle, CollapsingState>>(builder: (_, state) {
+        bool isCollapsed =
+            state.value == CollapsingState.COLLAPSED;
+        bool isVisible =
+            isCollapsed || state.key == titleType;
+        var width
+        return AnimatedOpacity(
+          duration: Duration(milliseconds: 200),
+          opacity: isVisible ? 1 : 0,
+          child: AnimatedContainer(
+            color: Colors.green,
+            duration: Duration(milliseconds: 200),
+            alignment: isCollapsed ? Alignment.center : Alignment.centerLeft,
             child: Container(
-              height: MediaQuery.of(context).size.height - 160,
-              margin: EdgeInsets.only(left: 500),
+              color: Colors.blue,
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.min,
                 children: [
-                Text("title", style: AppStyles.TITLE,),
-                Text("description description description description description description description description "
-                    "description description description description description description ")
-              ],),
+                  AnimatedContainer(
+                    duration: Duration(milliseconds: 200),
+                    width: width,
+                    height: height,
+                    child: InkWell(
+                        onTap: () {
+                          setState(() {
+                            isActive = !isActive;
+                            if (isActive) {
+                              width = 400;
+                              height = 400;
+                            } else {
+                              width = 150;
+                              height = 100;
+                            }
+                          });
+                        },
+                        child: Image.asset(
+                          assetName,
+                          fit: BoxFit.contain,
+                        )),
+                  ),
+                  AnimatedOpacity(
+                      opacity: isVisible ? 0 : 1,
+                      duration: Duration(milliseconds: 100),
+                      child: Text(
+                        "title",
+                        style: AppStyles.TITLE,
+                      ))
+                ],
+              ),
             ),
-          )
-        ],
-      ),
-    );
+          ),
+        );
+      });
+    }
+
+    return BlocBuilder<MetricsBloc, Metrics>(builder: (_, state) {
+      bool isMouse = state == Metrics.BIG;
+      return BlocBuilder<CollapsedHeadersConsBloc,
+          MapEntry<CollapsingTitle, CollapsingState>>(
+        builder: (_, state) {
+          bool isDetailsVisible = state.value == CollapsingState.EXPANDED;
+          return Container(
+            child: Stack(
+              children: [
+                Container(
+                  child: Column(
+                    children: [
+                      _buildPicture(CollapsingTitle.CONS_1),
+                    ],
+                  ),
+                ),
+                AnimatedOpacity(
+                  opacity: isDetailsVisible ? 1 : 0,
+                  duration: Duration(milliseconds: 100),
+                  child: Container(
+                    margin: EdgeInsets.only(left: 500),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "title",
+                          style: AppStyles.TITLE,
+                        ),
+                        Text(
+                            "description description description description description description description description "
+                            "description description description description description description ")
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          );
+        }
+      );
+    });
   }
 
   @override
   void initState() {
     super.initState();
-    isActive = false;
-    isActive1 = false;
-    width = 150;
-    height = 100;
-    width1 = 150;
-    height1 = 100;
   }
 }
